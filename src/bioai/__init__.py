dd9a8799b44428588e7f400d84e3300a5b77f386
Chabel Holdo BOSSA <142637082+chabelbossa@users.noreply.github.com>
2026-08-18 00:14:57 +0100

    feat: bootstrap reusable bioai package

---
+++ src/bioai/__init__.py
@@ -0,0 +1,6 @@
+"""Reusable core of the EEIA 2026 bioAI project."""
+
+from .features import FEATURE_DIM, FEATURE_VERSION
+
+__all__ = ["FEATURE_DIM", "FEATURE_VERSION", "__version__"]
+__version__ = "0.1.0"
